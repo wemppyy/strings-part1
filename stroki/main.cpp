06e62492807cad1b3ef04b0150d4bed61df8a40c
wemppyy <denisfrostaliuk@gmail.com>
2024-10-12 01:48:12 +0300

    done

--- stroki/main.cpp
+++ stroki/main.cpp
@@ -2,6 +2,28 @@
 
 using namespace std;
 
+void remove_symbol(char*& str, char symbol) {
+	int len = strlen(str);
+	char* new_str = new char[len + 1];
+
+	int j = 0;  // Индекс для новой строки
+	for (int i = 0; i < len; i++) {
+		if (str[i] != symbol) {
+			new_str[j] = str[i];
+			j++;
+		}
+	}
+
+	new_str[j] = '\0';
+
+	delete[] str;
+	str = new_str;
+}
+
 int main() {
+	char* str_p = new char[100] {"Hello World"};
+	cout << str_p << endl;
+	remove_symbol(str_p, 'o');
+	cout << str_p << endl;
 	return 0;
 }
