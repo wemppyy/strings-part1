adff14e1ce19c5e3c4a10c545132dace7360ccc8
wemppyy <denisfrostaliuk@gmail.com>
2024-10-12 01:57:03 +0300

    done

--- stroki/main.cpp
+++ stroki/main.cpp
@@ -2,6 +2,28 @@
 
 using namespace std;
 
+void str_replace(char*& str) {
+	int len = strlen(str);
+	for (size_t i = 0; i < len; i++)
+	{
+		if (str[i] == '.') str[i] = '!';
+	}
+}
+
 int main() {
+	int len;
+	cout << "Enter the length of the string: ";
+	cin >> len;
+
+	char* str = new char[len + 1];
+	cout << "Enter the string: ";
+	cin >> str;
+
+	system("pause");
+
+	str_replace(str);
+
+	cout << str << endl;
+
 	return 0;
 }
