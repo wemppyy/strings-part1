5728e1ebe798de01913a3683cd5a313ff45023bb
wemppyy <denisfrostaliuk@gmail.com>
2024-10-12 02:06:48 +0300

    done

--- stroki/main.cpp
+++ stroki/main.cpp
@@ -2,6 +2,26 @@
 
 using namespace std;
 
+void str_replace(char*& str) {
+	int len = strlen(str);
+
+	for (size_t i = 0; i < len; i++)
+	{
+		if (str[i] == ' ') str[i] = '\t';
+	}
+}
+
 int main() {
+	char* str = new char[100] {"Hello world!"};
+	cout << str << endl;
+
+	system("pause");
+
+	str_replace(str);
+
+	cout << str << endl;
+
+	delete[] str;
+
 	return 0;
 }
