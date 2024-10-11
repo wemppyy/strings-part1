1cbf869a7cce65d01f938b87e8fda32084c30c84
wemppyy <denisfrostaliuk@gmail.com>
2024-10-12 02:24:23 +0300

    done

--- stroki/main.cpp
+++ stroki/main.cpp
@@ -2,6 +2,33 @@
 
 using namespace std;
 
+bool is_palindrome(char* str) {
+	int len = strlen(str);
+	for (size_t i = 0; i < len; i++)
+	{
+		if (str[i] != str[len - i - 1]) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
 int main() {
+	char* str = new char[100] {"12321"};
+
+	cout << str << endl;
+
+	system("pause");
+
+	if (is_palindrome(str)) {
+		cout << "Yes" << endl;
+	}
+	else {
+		cout << "No" << endl;
+	}
+
+	delete[] str;
+
 	return 0;
 }
