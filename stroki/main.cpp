f7625c8a78f7101cb3db73c9fac501fe39db6a51
wemppyy <denisfrostaliuk@gmail.com>
2024-10-12 01:33:02 +0300

    done

--- stroki/main.cpp
+++ stroki/main.cpp
@@ -2,6 +2,27 @@
 
 using namespace std;
 
+template <typename T>
+void remove(T* &str, int index) {
+	int len = strlen(str);
+	T* new_str = new T[len];
+	for (size_t i = 0; i < index; i++)
+	{
+		new_str[i] = str[i];
+	}
+	for (size_t i = index; i < len; i++)
+	{
+		new_str[i] = str[i + 1];
+	}
+	delete[] str;
+	str = new_str;
+}
+
+
 int main() {
-	return 0;
+    char* str_p = new char[20] {"Hello World"};
+	cout << str_p << endl;
+	remove(str_p, 6);
+	cout << str_p << endl;
+    return 0;
 }
