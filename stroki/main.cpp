c0c20a2f5c3787835f20b588ba16185df02d569a
wemppyy <denisfrostaliuk@gmail.com>
2024-10-12 02:08:56 +0300

    done

--- stroki/main.cpp
+++ stroki/main.cpp
@@ -2,6 +2,50 @@
 
 using namespace std;
 
+int get_nums_count(char* str) {
+	int count = 0;
+	for (int i = 0; str[i] != '\0'; i++) {
+		if (str[i] >= '0' && str[i] <= '9') {
+			count++;
+		}
+	}
+	return count;
+}
+
+int get_letters_count(char* str) {
+	int count = 0;
+	for (int i = 0; str[i] != '\0'; i++) {
+		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
+			count++;
+		}
+	}
+	return count;
+}
+
+int get_other_count(char* str) {
+	int count = 0;
+	for (int i = 0; str[i] != '\0'; i++) {
+		if (!((str[i] >= '0' && str[i] <= '9') || (str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))) {
+			count++;
+		}
+	}
+	return count;
+}
+
+void print_string_info(char* str) {
+	cout << "Numbers count: " << get_nums_count(str) << endl;
+	cout << "Letters count: " << get_letters_count(str) << endl;
+	cout << "Other symbols count: " << get_other_count(str) << endl;
+}
+
 int main() {
+	char* str = new char[100] {"Hello world!"};
+	cout << str << endl;
+
+	system("pause");
+
+	print_string_info(str);
+
+	delete[] str;
 	return 0;
 }
